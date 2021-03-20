caf396fbb75239f839dc9acf6ad9363704ae57ed
carolsantoss <66979707+carolsantoss@users.noreply.github.com>
2021-03-20 13:33:35 -0300

    upload code

---
+++ app/src/main/java/com/example/appmuseu/exibicoes.java
@@ -0,0 +1,14 @@
+package com.example.appmuseu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exibicoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exibicoes);
+    }
+}
