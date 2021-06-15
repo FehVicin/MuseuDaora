d36890baf87cac2b4169dd503217cb9a494d3623
carolsantoss <66979707+carolsantoss@users.noreply.github.com>
2021-06-15 13:42:35 -0300

    view customizada

--- app/src/main/java/com/example/appmuseu/MainActivity.java
+++ app/src/main/java/com/example/appmuseu/MainActivity.java
@@ -3,15 +3,19 @@ package com.example.appmuseu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txttitulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txttitulo = findViewById(R.id.txttitulo);
     }
 
     public void clickhorario (View view) {
@@ -33,4 +37,30 @@ public class MainActivity extends AppCompatActivity {
         Intent intent5 = new Intent(this, Exibicoes.class);
         startActivity(intent5);
     }
-}
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        outState.putInt("COR",txttitulo.getCurrentTextColor());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        int color = savedInstanceState.getInt("COR");
+        txttitulo.setTextColor(color);
+
+    }
+
+    public void mudarcor (View view){
+        TextView txt = (TextView)view;
+        if(txt.getCurrentTextColor()!= Color.RED)
+        {
+            txt.setTextColor(Color.RED);
+        }
+        else
+        {
+            txt.setTextColor(Color.BLACK);
+        }
