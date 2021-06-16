2aebabcdaa5b926b4e132eacccdba47da91b4a75
carolsantoss <66979707+carolsantoss@users.noreply.github.com>
2021-06-15 22:41:09 -0300

    sensornovo

---
+++ app/src/main/java/com/example/appmuseu/SensorActivity.java
@@ -0,0 +1,52 @@
+package com.example.appmuseu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private TextView sensorluz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+        sensorluz = findViewById(R.id.txtsensor);
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        lightSensor = (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT));
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float light = event.values[0];
+        sensorluz.setText(String.valueOf(light));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
